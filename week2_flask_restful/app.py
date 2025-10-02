181ba1d3b97f58ec82acbbfd879ba4181f8625b1
NguyenVanHung2004 <22028118@vnu.edu.vn>
2025-10-02 13:24:46 +0700

    fix: small stuff and add comment

--- week2_flask_restful/app.py
+++ week2_flask_restful/app.py
@@ -1,6 +1,8 @@
 #python -m venv .venv
 #.venv\Scripts\activate
 #pip install -r requirements.txt
+#python app.py
+# api at: http://127.0.0.1:5000/docs
 from datetime import datetime, timedelta
 from dateutil import tz
 from flask import Flask, request, jsonify, make_response
